d88bd5a0872f2be24a8c19d6442ba53103dee60c
Your Name <you@example.com>
2020-07-25 20:55:41 +0800

    a

--- wide_deep/torch_model.py
+++ wide_deep/torch_model.py
@@ -19,6 +19,7 @@ class WideDeepLoader(Dataset):
     --------
     data: namedtuple with 3 elements - (wide_input_data, deep_inp_data, target)
     """
+
     def __init__(self, data):
 
         self.X_wide = data.wide
@@ -29,7 +30,7 @@ class WideDeepLoader(Dataset):
 
         xw = self.X_wide[idx]
         xd = self.X_deep[idx]
-        y  = self.Y[idx]
+        y = self.Y[idx]
 
         return xw, xd, y
 
@@ -79,22 +80,25 @@ class WideDeep(nn.Module):
         self.n_class = n_class
 
         # Build the embedding layers to be passed through the deep-side
-        for col,val,dim in self.embeddings_input:
+        for col, val, dim in self.embeddings_input:
             setattr(self, 'emb_layer_'+col, nn.Embedding(val, dim))
 
         # Build the deep-side hidden layers with dropout if specified
         input_emb_dim = np.sum([emb[2] for emb in self.embeddings_input])
-        self.linear_1 = nn.Linear(input_emb_dim+len(continuous_cols), self.hidden_layers[0])
+        self.linear_1 = nn.Linear(
+            input_emb_dim+len(continuous_cols), self.hidden_layers[0])
         if self.dropout:
             self.linear_1_drop = nn.Dropout(self.dropout[0])
-        for i,h in enumerate(self.hidden_layers[1:],1):
-            setattr(self, 'linear_'+str(i+1), nn.Linear( self.hidden_layers[i-1], self.hidden_layers[i] ))
+        for i, h in enumerate(self.hidden_layers[1:], 1):
+            setattr(self, 'linear_'+str(i+1),
+                    nn.Linear(self.hidden_layers[i-1], self.hidden_layers[i]))
             if self.dropout:
-                setattr(self, 'linear_'+str(i+1)+'_drop', nn.Dropout(self.dropout[i]))
+                setattr(self, 'linear_'+str(i+1)+'_drop',
+                        nn.Dropout(self.dropout[i]))
 
         # Connect the wide- and dee-side of the model to the output neuron(s)
-        self.output = nn.Linear(self.hidden_layers[-1]+self.wide_dim, self.n_class)
-
+        self.output = nn.Linear(
+            self.hidden_layers[-1]+self.wide_dim, self.n_class)
 
     def compile(self, method="logistic", optimizer="Adam", learning_rate=0.001, momentum=0.0):
         """Wrapper to set the activation, loss and the optimizer.
@@ -112,14 +116,16 @@ class WideDeep(nn.Module):
             self.activation, self.criterion = F.softmax, F.cross_entropy
 
         if optimizer == "Adam":
-            self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+            self.optimizer = torch.optim.Adam(
+                self.parameters(), lr=learning_rate)
         if optimizer == "RMSprop":
-            self.optimizer = torch.optim.RMSprop(self.parameters(), lr=learning_rate)
+            self.optimizer = torch.optim.RMSprop(
+                self.parameters(), lr=learning_rate)
         if optimizer == "SGD":
-            self.optimizer = torch.optim.SGD(self.parameters(), lr=learning_rate, momentum=momentum)
+            self.optimizer = torch.optim.SGD(
+                self.parameters(), lr=learning_rate, momentum=momentum)
 
         self.method = method
-
 
     def forward(self, X_w, X_d):
         """Implementation of the forward pass.
@@ -134,10 +140,11 @@ class WideDeep(nn.Module):
         out (torch.tensor) : result of the output neuron(s)
         """
         # Deep Side
-        emb = [getattr(self, 'emb_layer_'+col)(X_d[:,self.deep_column_idx[col]].long())
-               for col,_,_ in self.embeddings_input]
+        emb = [getattr(self, 'emb_layer_'+col)(X_d[:, self.deep_column_idx[col]].long())
+               for col, _, _ in self.embeddings_input]
         if self.continuous_cols:
-            cont_idx = [self.deep_column_idx[col] for col in self.continuous_cols]
+            cont_idx = [self.deep_column_idx[col]
+                        for col in self.continuous_cols]
             cont = [X_d[:, cont_idx].float()]
             deep_inp = torch.cat(emb+cont, 1)
         else:
@@ -146,8 +153,8 @@ class WideDeep(nn.Module):
         x_deep = F.relu(self.linear_1(deep_inp))
         if self.dropout:
             x_deep = self.linear_1_drop(x_deep)
-        for i in range(1,len(self.hidden_layers)):
-            x_deep = F.relu( getattr(self, 'linear_'+str(i+1))(x_deep) )
+        for i in range(1, len(self.hidden_layers)):
+            x_deep = F.relu(getattr(self, 'linear_'+str(i+1))(x_deep))
             if self.dropout:
                 x_deep = getattr(self, 'linear_'+str(i+1)+'_drop')(x_deep)
 
@@ -160,7 +167,6 @@ class WideDeep(nn.Module):
             out = self.activation(self.output(wide_deep_input))
 
         return out
-
 
     def fit(self, dataset, n_epochs, batch_size):
         """Run the model for the training set at dataset.
@@ -180,37 +186,41 @@ class WideDeep(nn.Module):
         # set the model in training mode
         net = self.train()
         for epoch in range(n_epochs):
-            total=0
-            correct=0
+            total = 0
+            correct = 0
             for i, (X_wide, X_deep, target) in enumerate(train_loader):
                 X_w = Variable(X_wide)
                 X_d = Variable(X_deep)
-                y = (Variable(target).float() if self.method != 'multiclass' else Variable(target))
+                y = (Variable(target).float() if self.method !=
+                     'multiclass' else Variable(target))
 
                 if use_cuda:
-                    X_w, X_d, y = X_w.cuda(), X_d.cuda(), y.cuda()
+                    X_w = X_w.to("cuda:9")
+                    X_d = X_d.to("cuda:9")
+                    y = y.to("cuda:9")
+
+                    # X_w, X_d, y = X_w.cuda(), X_d.cuda(), y.cuda()
 
                 self.optimizer.zero_grad()
-                y_pred =  net(X_w, X_d)
+                y_pred = net(X_w, X_d)
                 loss = self.criterion(y_pred, y)
                 loss.backward()
                 self.optimizer.step()
 
                 if self.method != "regression":
-                    total+= y.size(0)
+                    total += y.size(0)
                     if self.method == 'logistic':
                         y_pred_cat = (y_pred > 0.5).squeeze(1).float()
                     if self.method == "multiclass":
                         _, y_pred_cat = torch.max(y_pred, 1)
-                    correct+= float((y_pred_cat == y).sum().data[0])
+                    correct += float((y_pred_cat == y).sum().data[0])
 
             if self.method != "regression":
-                print ('Epoch {} of {}, Loss: {}, accuracy: {}'.format(epoch+1,
-                    n_epochs, round(loss.data[0],3), round(correct/total,4)))
+                print('Epoch {} of {}, Loss: {}, accuracy: {}'.format(epoch+1,
+                                                                      n_epochs, round(loss.data[0], 3), round(correct/total, 4)))
             else:
-                print ('Epoch {} of {}, Loss: {}'.format(epoch+1, n_epochs,
-                    round(loss.data[0],3)))
-
+                print('Epoch {} of {}, Loss: {}'.format(epoch+1, n_epochs,
+                                                        round(loss.data[0], 3)))
 
     def predict(self, dataset):
         """Predict target for dataset.
@@ -229,11 +239,13 @@ class WideDeep(nn.Module):
         X_d = Variable(torch.from_numpy(dataset.deep))
 
         if use_cuda:
-            X_w, X_d = X_w.cuda(), X_d.cuda()
+            X_w = X_w.to("cuda:9")
+            X_d = X_d.to("cuda:9")
+            # X_w, X_d = X_w.cuda(), X_d.cuda()
 
         # set the model in evaluation mode so dropout is not applied
         net = self.eval()
-        pred = net(X_w,X_d).cpu()
+        pred = net(X_w, X_d).cpu()
         if self.method == "regression":
             return pred.squeeze(1).data.numpy()
         if self.method == "logistic":
@@ -241,7 +253,6 @@ class WideDeep(nn.Module):
         if self.method == "multiclass":
             _, pred_cat = torch.max(pred, 1)
             return pred_cat.data.numpy()
-
 
     def predict_proba(self, dataset):
         """Predict predict probability for dataset.
@@ -261,20 +272,21 @@ class WideDeep(nn.Module):
         X_d = Variable(torch.from_numpy(dataset.deep))
 
         if use_cuda:
-            X_w, X_d = X_w.cuda(), X_d.cuda()
+            X_w = X_w.to("cuda:9")
+            X_d = X_d.to("cuda:9")
+            # X_w, X_d = X_w.cuda(), X_d.cuda()
 
         # set the model in evaluation mode so dropout is not applied
         net = self.eval()
-        pred = net(X_w,X_d).cpu()
+        pred = net(X_w, X_d).cpu()
         if self.method == "logistic":
             pred = pred.squeeze(1).data.numpy()
-            probs = np.zeros([pred.shape[0],2])
-            probs[:,0] = 1-pred
-            probs[:,1] = pred
+            probs = np.zeros([pred.shape[0], 2])
+            probs[:, 0] = 1-pred
+            probs[:, 1] = pred
             return probs
         if self.method == "multiclass":
             return pred.data.numpy()
-
 
     def get_embeddings(self, col_name):
         """Extract the embeddings for the embedding columns.
@@ -290,14 +302,12 @@ class WideDeep(nn.Module):
 
         params = list(self.named_parameters())
         emb_layers = [p for p in params if 'emb_layer' in p[0]]
-        emb_layer  = [layer for layer in emb_layers if col_name in layer[0]][0]
+        emb_layer = [layer for layer in emb_layers if col_name in layer[0]][0]
         embeddings = emb_layer[1].cpu().data.numpy()
         col_label_encoding = self.encoding_dict[col_name]
-        inv_dict = {v:k for k,v in col_label_encoding.iteritems()}
+        inv_dict = {v: k for k, v in col_label_encoding.iteritems()}
         embeddings_dict = {}
-        for idx,value in inv_dict.iteritems():
+        for idx, value in inv_dict.iteritems():
             embeddings_dict[value] = embeddings[idx]
 
         return embeddings_dict
-
-
