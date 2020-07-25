e73de03e1cc74405f0bd24725caa29304907a233
Your Name <you@example.com>
2020-07-25 20:58:09 +0800

    a

--- wide_deep/data_utils.py
+++ wide_deep/data_utils.py
@@ -34,7 +34,7 @@ def label_encode(df, cols=None):
         val_types[c] = df[c].unique()
 
     val_to_idx = dict()
-    for k, v in val_types.iteritems():
+    for k, v in val_types.items():
         val_to_idx[k] = {o: i for i, o in enumerate(val_types[k])}
 
     for k, v in val_to_idx.iteritems():
@@ -44,8 +44,7 @@ def label_encode(df, cols=None):
 
 
 def prepare_data(df, wide_cols, crossed_cols, embeddings_cols, continuous_cols, target,
-    scale=False, def_dim=8, seed=1981):
-
+                 scale=False, def_dim=8, seed=1981):
     """Prepares a pandas dataframe for the WideDeep model.
 
     Parameters:
@@ -82,7 +81,7 @@ def prepare_data(df, wide_cols, crossed_cols, embeddings_cols, continuous_cols, 
         emb_dim = dict(embeddings_cols)
         embeddings_cols = [emb[0] for emb in embeddings_cols]
     else:
-        emb_dim = {e:def_dim for e in embeddings_cols}
+        emb_dim = {e: def_dim for e in embeddings_cols}
     deep_cols = embeddings_cols+continuous_cols
 
     # Extract the target and copy the dataframe so we don't mutate it
@@ -99,49 +98,54 @@ def prepare_data(df, wide_cols, crossed_cols, embeddings_cols, continuous_cols, 
         crossed_columns.append(colname)
 
     # Extract the categorical column names that can be one hot encoded later
-    categorical_columns = list(df_tmp.select_dtypes(include=['object']).columns)
+    categorical_columns = list(
+        df_tmp.select_dtypes(include=['object']).columns)
 
     # Encode the dataframe and get the encoding Dictionary only for the
     # deep_cols (for the wide_cols is uneccessary)
-    encoding_dict,df_tmp = label_encode(df_tmp)
-    encoding_dict = {k:encoding_dict[k] for k in encoding_dict if k in deep_cols}
+    encoding_dict, df_tmp = label_encode(df_tmp)
+    encoding_dict = {k: encoding_dict[k]
+                     for k in encoding_dict if k in deep_cols}
     embeddings_input = []
-    for k,v in encoding_dict.iteritems():
+    for k, v in encoding_dict.iteritems():
         embeddings_input.append((k, len(v), emb_dim[k]))
 
     # select the deep_cols and get the column index that will be use later
     # to slice the tensors
     df_deep = df_tmp[deep_cols]
-    deep_column_idx = {k:v for v,k in enumerate(df_deep.columns)}
+    deep_column_idx = {k: v for v, k in enumerate(df_deep.columns)}
 
     # The continous columns will be concatenated with the embeddings, so you
     # probably want to normalize them first
     if scale:
         scaler = StandardScaler()
         for cc in continuous_cols:
-            df_deep[cc]  = scaler.fit_transform(df_deep[cc].values.reshape(-1,1))
+            df_deep[cc] = scaler.fit_transform(
+                df_deep[cc].values.reshape(-1, 1))
 
     # select the wide_cols and one-hot encode those that are categorical
     df_wide = df_tmp[wide_cols+crossed_columns]
     del(df_tmp)
-    dummy_cols = [c for c in wide_cols+crossed_columns if c in categorical_columns]
+    dummy_cols = [c for c in wide_cols +
+                  crossed_columns if c in categorical_columns]
     df_wide = pd.get_dummies(df_wide, columns=dummy_cols)
 
     # train/test split
-    X_train_deep, X_test_deep = train_test_split(df_deep.values, test_size=0.3, random_state=seed)
-    X_train_wide, X_test_wide = train_test_split(df_wide.values, test_size=0.3, random_state=seed)
+    X_train_deep, X_test_deep = train_test_split(
+        df_deep.values, test_size=0.3, random_state=seed)
+    X_train_wide, X_test_wide = train_test_split(
+        df_wide.values, test_size=0.3, random_state=seed)
     y_train, y_test = train_test_split(Y, test_size=0.3, random_state=1981)
 
     # Building the output dictionary
     wd_dataset = dict()
     train_dataset = namedtuple('train_dataset', 'wide, deep, labels')
-    test_dataset  = namedtuple('test_dataset' , 'wide, deep, labels')
-    wd_dataset['train_dataset'] = train_dataset(X_train_wide, X_train_deep, y_train)
-    wd_dataset['test_dataset']  = test_dataset(X_test_wide, X_test_deep, y_test)
-    wd_dataset['embeddings_input']  = embeddings_input
+    test_dataset = namedtuple('test_dataset', 'wide, deep, labels')
+    wd_dataset['train_dataset'] = train_dataset(
+        X_train_wide, X_train_deep, y_train)
+    wd_dataset['test_dataset'] = test_dataset(X_test_wide, X_test_deep, y_test)
+    wd_dataset['embeddings_input'] = embeddings_input
     wd_dataset['deep_column_idx'] = deep_column_idx
     wd_dataset['encoding_dict'] = encoding_dict
 
     return wd_dataset
-
-
